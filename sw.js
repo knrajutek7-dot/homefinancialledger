6d8d24e77c6d6c0c02913db6373115ac67ee83d5
knrajutek7-dot <knrajutek7@gmail.com>
2026-08-16 12:08:11 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'knr-ledger-v2';
-const SHELL = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
+const SHELL = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png', './logo-wide.png'];
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
